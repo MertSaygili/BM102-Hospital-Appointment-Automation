e0a83521c88f7dd2aa267762e39e2b9fac5ed564
Seda Nur Taşkan <snur.taskan@gazi.edu.tr>
2021-05-22 14:41:25 +0300

    Şikayet ve öneri paneli yapıldı.

---
+++ BM102Proje/K.Sikayet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace BM102Proje
+{
+    public partial class Sikayetİstek : Form
+    {
+        OleDbConnection baglantı = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\\..\\veriler\\veritabani_access.mdb");
+        public Sikayetİstek()
+        {
+            InitializeComponent();
+        }
+
+        private void GeriDon_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void onaylabutton_Click(object sender, EventArgs e)
+        {
+            if(SikayetKonu.Text != "" && SikayetSikayet.Text != "")
+            {
+                baglantı.Open();
+                string ad = KullanıcıMenü.ad;
+                string soyad = KullanıcıMenü.soyad;
+                OleDbCommand komut = new OleDbCommand("insert into Sikayetler (Isim,Soyisim,Konu,Icerik) values (@p1,@p2,@p3,@p4)", baglantı);
+                komut.Parameters.AddWithValue("@p1", ad);
+                komut.Parameters.AddWithValue("@p2", soyad);
+                komut.Parameters.AddWithValue("@p3", SikayetKonu.Text);
+                komut.Parameters.AddWithValue("@p4", SikayetSikayet.Text);
+                komut.ExecuteNonQuery();
+                baglantı.Close();
+                MessageBox.Show("Talebiniz başarıyla iletilmiştir.\nSağlıklı günler!");
+            }
+        }
+    }
+}
