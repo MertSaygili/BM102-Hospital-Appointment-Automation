707fc6c3979d59cfa3064e90d0e67eaa83c05da6
Seda Nur Taşkan <snur.taskan@gazi.edu.tr>
2021-05-24 17:56:25 +0300

    Duyuru kısmı tamamlandı.

--- BM102Proje/Y.DuyuruEkleme.Designer.cs
+++ BM102Proje/Y.DuyuruEkleme.Designer.cs
@@ -33,18 +33,29 @@ namespace BM102Proje
             this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.onaylabutton = new Bunifu.Framework.UI.BunifuImageButton();
             this.DuyuruEkleText = new System.Windows.Forms.RichTextBox();
-            this.SikayetSikayetText = new System.Windows.Forms.Label();
+            this.duyurutext = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.duyurucikarma = new System.Windows.Forms.ComboBox();
+            this.onaylabuton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.cikarma = new System.Windows.Forms.Button();
+            this.ekleme = new System.Windows.Forms.Button();
             this.bunifuGradientPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.onaylabutton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onaylabuton1)).BeginInit();
             this.SuspendLayout();
             // 
             // bunifuGradientPanel1
             // 
             this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
             this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.ekleme);
+            this.bunifuGradientPanel1.Controls.Add(this.cikarma);
+            this.bunifuGradientPanel1.Controls.Add(this.onaylabuton1);
+            this.bunifuGradientPanel1.Controls.Add(this.duyurucikarma);
+            this.bunifuGradientPanel1.Controls.Add(this.label1);
             this.bunifuGradientPanel1.Controls.Add(this.onaylabutton);
             this.bunifuGradientPanel1.Controls.Add(this.DuyuruEkleText);
-            this.bunifuGradientPanel1.Controls.Add(this.SikayetSikayetText);
+            this.bunifuGradientPanel1.Controls.Add(this.duyurutext);
             this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.LightSeaGreen;
             this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.LightCyan;
             this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.LightSeaGreen;
@@ -53,7 +64,7 @@ namespace BM102Proje
             this.bunifuGradientPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
             this.bunifuGradientPanel1.Quality = 10;
-            this.bunifuGradientPanel1.Size = new System.Drawing.Size(670, 444);
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(463, 252);
             this.bunifuGradientPanel1.TabIndex = 0;
             // 
             // onaylabutton
@@ -61,7 +72,7 @@ namespace BM102Proje
             this.onaylabutton.BackColor = System.Drawing.Color.Transparent;
             this.onaylabutton.Image = ((System.Drawing.Image)(resources.GetObject("onaylabutton.Image")));
             this.onaylabutton.ImageActive = null;
-            this.onaylabutton.Location = new System.Drawing.Point(231, 194);
+            this.onaylabutton.Location = new System.Drawing.Point(402, 191);
             this.onaylabutton.Name = "onaylabutton";
             this.onaylabutton.Size = new System.Drawing.Size(50, 45);
             this.onaylabutton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -73,31 +84,85 @@ namespace BM102Proje
             // DuyuruEkleText
             // 
             this.DuyuruEkleText.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.DuyuruEkleText.Location = new System.Drawing.Point(81, 31);
+            this.DuyuruEkleText.Location = new System.Drawing.Point(118, 103);
             this.DuyuruEkleText.Margin = new System.Windows.Forms.Padding(2);
             this.DuyuruEkleText.Name = "DuyuruEkleText";
-            this.DuyuruEkleText.Size = new System.Drawing.Size(200, 158);
+            this.DuyuruEkleText.Size = new System.Drawing.Size(335, 83);
             this.DuyuruEkleText.TabIndex = 64;
             this.DuyuruEkleText.Text = "";
             // 
-            // SikayetSikayetText
+            // duyurutext
             // 
-            this.SikayetSikayetText.AutoSize = true;
-            this.SikayetSikayetText.BackColor = System.Drawing.Color.Transparent;
-            this.SikayetSikayetText.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.SikayetSikayetText.Location = new System.Drawing.Point(17, 31);
-            this.SikayetSikayetText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.SikayetSikayetText.Name = "SikayetSikayetText";
-            this.SikayetSikayetText.Size = new System.Drawing.Size(79, 22);
-            this.SikayetSikayetText.TabIndex = 63;
-            this.SikayetSikayetText.Text = "Duyuru:";
+            this.duyurutext.AutoSize = true;
+            this.duyurutext.BackColor = System.Drawing.Color.Transparent;
+            this.duyurutext.Font = new System.Drawing.Font("Cambria", 12F);
+            this.duyurutext.Location = new System.Drawing.Point(17, 103);
+            this.duyurutext.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.duyurutext.Name = "duyurutext";
+            this.duyurutext.Size = new System.Drawing.Size(79, 23);
+            this.duyurutext.TabIndex = 63;
+            this.duyurutext.Text = "Duyuru:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F);
+            this.label1.Location = new System.Drawing.Point(17, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 23);
+            this.label1.TabIndex = 68;
+            this.label1.Text = "İşlem:";
+            // 
+            // duyurucikarma
+            // 
+            this.duyurucikarma.FormattingEnabled = true;
+            this.duyurucikarma.Location = new System.Drawing.Point(118, 103);
+            this.duyurucikarma.Name = "duyurucikarma";
+            this.duyurucikarma.Size = new System.Drawing.Size(335, 31);
+            this.duyurucikarma.TabIndex = 69;
+            // 
+            // onaylabuton1
+            // 
+            this.onaylabuton1.BackColor = System.Drawing.Color.Transparent;
+            this.onaylabuton1.Image = ((System.Drawing.Image)(resources.GetObject("onaylabuton1.Image")));
+            this.onaylabuton1.ImageActive = null;
+            this.onaylabuton1.Location = new System.Drawing.Point(402, 191);
+            this.onaylabuton1.Name = "onaylabuton1";
+            this.onaylabuton1.Size = new System.Drawing.Size(50, 45);
+            this.onaylabuton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.onaylabuton1.TabIndex = 70;
+            this.onaylabuton1.TabStop = false;
+            this.onaylabuton1.Zoom = 10;
+            this.onaylabuton1.Click += new System.EventHandler(this.onaylabuton1_Click);
+            // 
+            // cikarma
+            // 
+            this.cikarma.Location = new System.Drawing.Point(298, 47);
+            this.cikarma.Name = "cikarma";
+            this.cikarma.Size = new System.Drawing.Size(154, 29);
+            this.cikarma.TabIndex = 72;
+            this.cikarma.Text = "Çıkartma";
+            this.cikarma.UseVisualStyleBackColor = true;
+            this.cikarma.Click += new System.EventHandler(this.cikarma_Click);
+            // 
+            // ekleme
+            // 
+            this.ekleme.Location = new System.Drawing.Point(118, 47);
+            this.ekleme.Name = "ekleme";
+            this.ekleme.Size = new System.Drawing.Size(154, 29);
+            this.ekleme.TabIndex = 73;
+            this.ekleme.Text = "Ekleme";
+            this.ekleme.UseVisualStyleBackColor = true;
+            this.ekleme.Click += new System.EventHandler(this.ekleme_Click);
             // 
             // DuyuruEkle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(296, 240);
+            this.ClientSize = new System.Drawing.Size(458, 245);
             this.Controls.Add(this.bunifuGradientPanel1);
             this.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -106,11 +171,12 @@ namespace BM102Proje
             this.MinimizeBox = false;
             this.Name = "DuyuruEkle";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Duyuru Ekleme Paneli";
+            this.Text = "Duyuru Düzenleme";
             this.Load += new System.EventHandler(this.DuyuruEkle_Load);
             this.bunifuGradientPanel1.ResumeLayout(false);
             this.bunifuGradientPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.onaylabutton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onaylabuton1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -120,6 +186,11 @@ namespace BM102Proje
         private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
         private Bunifu.Framework.UI.BunifuImageButton onaylabutton;
         private System.Windows.Forms.RichTextBox DuyuruEkleText;
-        private System.Windows.Forms.Label SikayetSikayetText;
+        private System.Windows.Forms.Label duyurutext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox duyurucikarma;
+        private Bunifu.Framework.UI.BunifuImageButton onaylabuton1;
+        private System.Windows.Forms.Button cikarma;
+        private System.Windows.Forms.Button ekleme;
     }
 }
--- BM102Proje/Y.DuyuruEkleme.cs
+++ BM102Proje/Y.DuyuruEkleme.cs
@@ -19,22 +19,66 @@ namespace BM102Proje
         {
             InitializeComponent();
         }
+        private void DuyuruEkle_Load(object sender, EventArgs e)
+        {
+            baglantı.Open();
+            OleDbCommand komut = new OleDbCommand("Select * from Duyurular", baglantı);
+            OleDbDataReader dr = komut.ExecuteReader();
 
+            while (dr.Read())
+            {
+                string duyuru = dr.GetString(0);             //duyuruları comboxın içine koyuyoruz.
+                duyurucikarma.Items.Add(duyuru);
+            }
+
+            baglantı.Close();
+            duyurutext.Hide();
+            DuyuruEkleText.Hide();
+            onaylabutton.Hide();
+            duyurucikarma.Hide();
+            onaylabuton1.Hide();
+        }
         private void onaylabutton_Click(object sender, EventArgs e)
         {
-            //onayla butonuna basılınca girdileri veritabanına kaydeder. Ardından panlei kapatır.
+            //onayla butonuna basılınca girdileri veritabanına kaydeder.
             baglantı.Open();
             OleDbCommand komut = new OleDbCommand("insert into Duyurular (Duyuru) values (@p1)", baglantı);
             komut.Parameters.AddWithValue("@p1", DuyuruEkleText.Text);
             komut.ExecuteNonQuery();
             baglantı.Close();
             MessageBox.Show("Duyuru sisteme başarıyla eklenmiştir.");
+            this.Close(); // paneli kapatır
+        }
+
+        private void onaylabuton1_Click(object sender, EventArgs e)
+        {
+            //onayla butonuna basılınca girilen sıradaki veriyi veritabanından siler. Sonra tekrardan başa dönderir.
+            string sira = duyurucikarma.SelectedItem.ToString();
+            baglantı.Open();
+            OleDbCommand komut = new OleDbCommand("Delete * From Duyurular Where Duyuru = @a1", baglantı);
+            komut.Parameters.AddWithValue("@a1", sira);
+            komut.ExecuteNonQuery();
+            baglantı.Close();
+            MessageBox.Show("Duyuru sistemden başarıyla silinmiştir.");
             this.Close();
         }
 
-        private void DuyuruEkle_Load(object sender, EventArgs e)
+        private void ekleme_Click(object sender, EventArgs e)
         {
+            duyurucikarma.Hide();
+            onaylabuton1.Hide();
+            duyurutext.Show();
+            DuyuruEkleText.Show();
+            onaylabutton.Show();
+        }
 
+        private void cikarma_Click(object sender, EventArgs e)
+        {
+            DuyuruEkleText.Hide();
+            onaylabutton.Hide();
+            duyurutext.Show();
+            duyurucikarma.Show();
+            onaylabuton1.Show();
         }
     }
 }
